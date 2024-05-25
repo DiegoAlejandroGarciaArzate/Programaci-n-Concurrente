7c1b866606a073deb3bdf84848d53c3e99af6610
“Joaquin <joaquingd1@hotmail.com>
2024-05-25 06:31:54 -0500

    Feat: Pruebas lectura csv

--- hola.go
+++ hola.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("HOLA MUNDO")
+
+	//resp, err := http.Get("https://raw.githubusercontent.com/iller15/TP-Concurrente/main/Secuencial.go")
+	resp, err := http.Get("https://github.com/DiegoAlejandroGarciaArzate/Programaci-n-Concurrente/blob/main/data/AEP_hourly.csv")
+	defer resp.Body.Close()
+
+	if err != nil {
+		fmt.Println("error \n", err)
+		return
+	}
+
+	body, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		fmt.Println("error \n", err)
+		return
+	}
+	fmt.Println(body[0])
 }
