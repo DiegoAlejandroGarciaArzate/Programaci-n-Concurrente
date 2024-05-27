dfe22f08234064840ff7a93b1de687f5ef1b1d4d
“Joaquin <joaquingd1@hotmail.com>
2024-05-27 02:57:08 -0500

    Feat: lee el dataset y da los valroes de x e y de entrenamiento en x_train e y_train

--- hola.go
+++ hola.go
@@ -1,28 +1,52 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
-	"io"
 	"net/http"
+	"strconv"
 )
 
 func main() {
-	fmt.Println("HOLA MUNDO")
 
-	//resp, err := http.Get("https://raw.githubusercontent.com/iller15/TP-Concurrente/main/Secuencial.go")
-	resp, err := http.Get("https://github.com/DiegoAlejandroGarciaArzate/Programaci-n-Concurrente/blob/main/data/AEP_hourly.csv")
+	var x_train, y_train []float64
+
+	resp, err := http.Get("https://raw.githubusercontent.com/DiegoAlejandroGarciaArzate/Programaci-n-Concurrente/main/taxi_data.csv")
 	defer resp.Body.Close()
 
 	if err != nil {
-		fmt.Println("error \n", err)
+		fmt.Println("error descarga csv \n", err)
 		return
 	}
 
-	body, err := io.ReadAll(resp.Body)
+	reader := csv.NewReader(resp.Body)
+
+	data, err := reader.ReadAll()
 
 	if err != nil {
-		fmt.Println("error \n", err)
+		fmt.Println("error abrir csv? \n", err)
 		return
 	}
-	fmt.Println(body[0])
+
+	fmt.Println("tamaño data:  ", len(data))
+
+	fmt.Println("Columns: ", data[0][0], "  ", data[0][1])
+
+	n := len(data) - 1
+	//Carga valores, si se hacen pruebas cambiar el len(data) a un número como 10
+	for i := 1; i < n; i++ {
+		trip, _ := strconv.ParseFloat(data[i][0], 64)
+		total, _ := strconv.ParseFloat(data[i][1], 64)
+
+		x_train = append(x_train, trip)
+		y_train = append(y_train, total)
+	}
+
+	//solo imprimir si el el valor de n es pequeño
+	/*for i := 0; i < len(x_train); i++ {
+		fmt.Println(x_train[i], y_train[i])
+
+	} */
+
+	fmt.Println("Tamñano arreglos: x = ", len(x_train), ";  y = ", len(y_train))
 }
